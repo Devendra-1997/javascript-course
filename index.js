b54262dfe278a9f5765b761f36328d68b4e5d119
Devendra-1997 <devendradhakal92@gmail.com>
2023-12-18 19:42:27 +1100

    first commit javascript

---
+++ index.js
@@ -0,0 +1,11 @@
+// variables
+
+// let name = "dev";
+// let age = 26;
+// let student = true;
+
+// console.log(name);
+// console.log(age);
+// console.log(student);
+
+// document.getElementById("p1").innerHTML = "you are " + age + "years old";
